d26a24e91e1856ff6db2e8382cb7bd9b0dc3d849
Tewodros <tewodrosabebe@gmail.com>
2022-06-30 16:50:52 -0700

    Hello Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,10 @@
 #include <string.h>
 
 /**
- *  * main - Entry point
- *   * Description: prints "\"Programming is like building a multilingual puzzle"*
- *    * Return: Always 0(Success)
- *     */
+ *main - Entry point
+ *Description: prints "\"Programming is like building a multilingual puzzle"*
+ *Return: Always 0(Success)
+ **/
 
 int main(void)
 {
