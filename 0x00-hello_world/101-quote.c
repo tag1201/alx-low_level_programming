59785cb6fa6be8ba41d55e02c2cb7bef091986c4
Tewodros <tewodrosabebe@gmail.com>
2022-06-30 18:29:05 -0700

    Quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,9 +3,9 @@
 
 /**
  *main - prints exactly "and that piece of art is useful"
- * - Dora Korpar, 2015-10-19"
+ * - Dora Korpar, 2015-10-19",
  * followed by a few line, to the standard error.
- *Return: Always 0 (Success)
+ * Return: Always 0 (Success)
 */
 int main(void)
 {
