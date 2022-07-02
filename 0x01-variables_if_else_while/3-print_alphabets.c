c29d2ab7de039dddd275bf24e4a832bffdb00550
Tewodros <tewodrosabebe@gmail.com>
2022-07-02 02:51:00 -0700

    alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ *main -prints the alphabet in lowercase and uppercase*
+ *followed by new line*
+
+ *return: 0
+**/
+int main(void)
+{
+	int ch;
+
+	for (ch = 97; ch <=122; ch++)
+	{
+	putchar(ch);
+	}
+	for (ch = 65; ch <= 90; ch++)
+	{
+	putchar(ch);
+	}
+	putchar(10); /*this is ASCII code for new line*/
+	return (0);
+}
